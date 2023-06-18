0c06a3393d58f5178bf677a4a4cea00f71be3790
Astrikkk <ukrvladit@gmail.com>
2023-06-18 21:25:21 +0300

    0.1
    main mechnic

---
+++ JobGame/Assets/Scripts/BlockController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private float speed;
+    private Collider2D col;
+
+    private void Start()
+    {
+        col = gameObject.GetComponent<Collider2D>();
+        col.isTrigger = true;
+    }
+
+    public void MoveToTarget(Vector3 target, float movementSpeed)
+    {
+        targetPosition = target;
+        speed = movementSpeed;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (transform.position == targetPosition)
+        {
+            enabled = false;
+            col.isTrigger = false;
+        }
+    }
+}
---
+++ JobGame/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Transform leftColumn; // Посилання на лівий стовб
+    public Transform rightColumn; // Посилання на правий стовб
+    public GameObject blockPrefab; // Префаб блоку
+    public float columnSpeed = 2f; // Швидкість руху стовбів
+    public float blockSpeed = 5f; // Швидкість руху блоків
+    public float blockSpawnRate = 1f; // Частота появи блоків
+    public int PlusHeightRight;
+    public int PlusHeightLeft;
+
+    private bool isOnLeftColumn = true; // Змінна, що вказує, чи персонаж знаходиться на лівому стовбі
+    private GameObject cam;
+
+    private float jumpDuration = 0.5f; // Тривалість прижка
+    private bool BlockPlace = true;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnBlock", 0f, blockSpawnRate);
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        cam.transform.Translate(Vector3.up * columnSpeed * Time.deltaTime);
+    }
+
+    private void MoveColumns()
+    {
+        if (leftColumn.position.y >= rightColumn.position.y)
+        {
+            rightColumn.position = new Vector3(rightColumn.position.x, leftColumn.position.y);
+        }
+        else
+        {
+            leftColumn.position = new Vector3(leftColumn.position.x, rightColumn.position.y);
+        }
+    }
+
+    private void SpawnBlock()
+    {
+        float randomX;
+        Vector3 targetPosition;
+
+        if (BlockPlace)
+        {
+            randomX = 5;
+            targetPosition = new Vector3(leftColumn.position.x, leftColumn.position.y + 2f + PlusHeightLeft);
+            BlockPlace = false;
+            PlusHeightLeft += 1;
+        }
+        else
+        {
+            randomX = -5;
+            targetPosition = new Vector3(rightColumn.position.x, rightColumn.position.y + 2f + PlusHeightRight);
+            BlockPlace = true;
+            PlusHeightRight += 1;
+        }
+
+        GameObject block = Instantiate(blockPrefab, new Vector3(randomX, leftColumn.position.y+1 + PlusHeightRight, 0f), Quaternion.identity);
+        block.GetComponent<BlockController>().MoveToTarget(targetPosition, blockSpeed);
+    }
+
+    private void Jump()
+    {
+        Vector3 targetPosition;
+
+        if (!isOnLeftColumn)
+        {
+            targetPosition = new Vector3(rightColumn.position.x, transform.position.y + 2);
+            isOnLeftColumn = true;
+        }
+        else
+        {
+            targetPosition = new Vector3(leftColumn.position.x, transform.position.y + 2);
+            isOnLeftColumn = false;
+        }
+
+        StartCoroutine(JumpRoutine(targetPosition));
+    }
+
+    private IEnumerator JumpRoutine(Vector3 targetPosition)
+    {
+        float elapsedTime = 0f;
+        Vector3 startingPosition = transform.position;
+
+        while (elapsedTime < jumpDuration)
+        {
+            float t = elapsedTime / jumpDuration;
+            transform.position = Vector3.Lerp(startingPosition, targetPosition, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+    }
+}
